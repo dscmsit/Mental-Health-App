5cc7e9e73f57485815f39b2e6d9f2ed952aa1f38
Harsh Agnihotri <harshagnihotri90@gmail.com>
2023-03-16 19:43:24 +0530

    login signup half done

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from scraper import fetch_req
+import csv
 
 app = Flask(__name__)
 
@@ -7,6 +8,7 @@ app = Flask(__name__)
 @app.route("/api")
 def hello_world():
     return 'Hello, World!'
+
 
 
 @app.route('/api_post', methods=['POST'])
@@ -25,6 +27,43 @@ def doctor():
         return "not working"
 
 
+@app.route("/login")
+def login():
+    data = request.get_json()
+    if users[data["email"]]:
+        if users[data["email"]] == data["pass"]:
+            return "Successfull"
+        else:
+            return "wrong password"
+    else:
+        return "user doesn't exist"
+    
 
-if __name__ == '__main__':
-    app.run()
+def update_data(username, email, password):
+    fieldnames = ["username", "password", "email"]
+
+    # open the CSV file in write mode
+    with open("output.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # write the header row with fieldnames
+        writer.writeheader()
+
+        # write each dictionary as a row in the CSV
+        for row in users:
+            writer.writerow(row)
+
+
+users = []
+with open('backend/data.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        d={}
+        d["username"] = row[0]
+        d["password"] = row[1]
+        d["email"] = row[2]
+        users.append(d)
+
+print(users)
+
+app.run()
--- backend/scraper.py
+++ backend/scraper.py
@@ -9,6 +9,7 @@ https://www.psychologyindia.com/state/Chandigarh-Therapist
 '''
 Uttar-Pradesh
 Chandigarh
+delhi
 '''
 
 def fetch_req(city):       
@@ -17,20 +18,22 @@ def fetch_req(city):
         headers={'User-Agent': 'Mozilla/5.0'}
     )
     response = {"name":"none","link":"none"}
-    html_code = urlopen(req).read().decode("utf-8")
-
-    # print(html_code)
-    start = html_code.find('<h3 class="title">') + len('<h3 class="title">')
-    end = html_code.find("</h3>")
-    name_find = html_code[start:end]
-    match = re.search(r'href=[\'"]?([^\'" >]+)', name_find)
-    if match:
-        response["link"]= match.group(1)
-    # print(name_find)
-    start = name_find.find('<b>') + len('<b>')
-    end = name_find.find('</b>')
-    if name_find[start:end]:
-        response["name"] = name_find[start:end]
+    try:
+        html_code = urlopen(req).read().decode("utf-8")
+        # print(html_code)
+        start = html_code.find('<h3 class="title">') + len('<h3 class="title">')
+        end = html_code.find("</h3>")
+        name_find = html_code[start:end]
+        match = re.search(r'href=[\'"]?([^\'" >]+)', name_find)
+        if match:
+            response["link"]= match.group(1)
+        # print(name_find)
+        start = name_find.find('<b>') + len('<b>')
+        end = name_find.find('</b>')
+        if name_find[start:end]:
+            response["name"] = name_find[start:end]
+    except:
+        html_code = ""
 
     return json.dumps(response)
 
