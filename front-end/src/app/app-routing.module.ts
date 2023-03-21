ae296c75006d25802adaa2dbf6491374a165de4e
Yuvraj <kingyuvi88@gmail.com>
2023-03-21 23:30:16 +0530

    Added routing.

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './Components/log-in/log-in.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'sign-in',
+    component:LogInComponent
+  },
+  {
+    path: 'sign-up',
+    component:SignUpComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
