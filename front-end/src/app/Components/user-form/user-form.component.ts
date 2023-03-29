4d98f39688eb35608309a1758d5c0cda9e11897e
Yuvraj <kingyuvi88@gmail.com>
2023-03-29 23:36:39 +0530

    Updated user-form

--- front-end/src/app/Components/user-form/user-form.component.ts
+++ front-end/src/app/Components/user-form/user-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-user-form',
@@ -7,4 +11,48 @@ import { Component } from '@angular/core';
 })
 export class UserFormComponent {
 
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor(public fb: FormBuilder, private http: HttpClient) {}
+  registrationForm = this.fb.group({
+    Age : ['', [Validators.required]],
+    Gender : ['', [Validators.required]],
+    family_history: ['', [Validators.required]],
+    no_employees: ['', [Validators.required]],
+    remote_work: ['', [Validators.required]],
+    benefits: ['', [Validators.required]],
+    care_options: ['', [Validators.required]],
+    wellness_program: ['', [Validators.required]],
+    seek_help: ['', [Validators.required]],
+    anonymity: ['', [Validators.required]],
+    leave: ['', [Validators.required]],
+    mental_health_consequence: ['', [Validators.required]],
+    coworkers: ['', [Validators.required]],
+
+
+  });
+  onSubmit(){
+    console.log("Form Submitted");
+     // console.warn(this.registrationForm.);
+     
+    const stringData = JSON.stringify(this.registrationForm.value); // data is a string you have to convert to json object
+    const data = JSON.parse(stringData); // data is now a JS object
+
+    const userData = {
+      header: 'register',
+      data: data,
+    };
+
+    this.http
+      .post('https://mentalhealthbackend.onrender.com/predict', userData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .subscribe((result) => {
+        console.log(result);
+      }); 
+  }
 }
