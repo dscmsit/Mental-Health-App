b8f41b405437b27392a55845959ec73b37ad2bc1
Saanvi Bhagat <sanvibhagat13@gmail.com>
2023-03-30 13:34:41 +0530

    profile page

--- front-end/src/app/Components/profile/profile.component.ts
+++ front-end/src/app/Components/profile/profile.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
-  constructor(){}
+export class ProfileComponent implements OnInit{
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void {
+      const id=localStorage.getItem('user_id')
+      console.warn(id)
+      this.http.get(`https://mentalhealthbackend.onrender.com/get_user/${id}`).subscribe((result)=>{
+        console.warn(result);
+      })
+  }
 
   LogOut(){
     localStorage.removeItem('login_status');
